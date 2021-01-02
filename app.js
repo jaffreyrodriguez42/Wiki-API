227c6fa3f16efd700d0024a5fafc37c973754dc8
Jaf <jaffreyrodriguez42@gmail.com>
2021-01-02 15:00:22 +0800

    Chained route handlers using express

--- app.js
+++ app.js
@@ -20,40 +20,40 @@ const articleSchema = ({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
-	Article.find({}, function(err, foundArticles){
-		if(!err){
-			res.send(foundArticles);
-		}else{
-			res.send(err);
-		}
-	});
-});
+app.route("/articles")
+	.get(function(req, res){
+		Article.find({}, function(err, foundArticles){
+			if(!err){
+				res.send(foundArticles);
+			}else{
+				res.send(err);
+			}
+		});
+	})
 
-app.post("/articles", function(req, res){
-	const newArticle = new Article({
-		title: req.body.title,
-		content: req.body.content
+	.post(function(req, res){
+		const newArticle = new Article({
+			title: req.body.title,
+			content: req.body.content
+		});
+		newArticle.save(function(err){
+			if(!err){
+				res.send("Successfully added new article!");
+			}else{
+				cres.send(err);
+			}
+		});
+	})
+	
+	.delete(function(req, res){
+		Article.deleteMany(function(err){
+			if(!err){
+				res.send("Successfully deleted all articles!");
+			}else{
+				res.send(err);
+			}
+		});
 	});
-
-	newArticle.save(function(err){
-		if(!err){
-			res.send("Successfully added new article!");
-		}else{
-			cres.send(err);
-		}
-	});
-});
-
-app.delete("/articles", function(req, res){
-	Article.deleteMany(function(err){
-		if(!err){
-			res.send("Successfully deleted all articles!");
-		}else{
-			res.send(err);
-		}
-	});
-});
 
 app.listen(3000, function(){
 	console.log("Server started on port 3000");
